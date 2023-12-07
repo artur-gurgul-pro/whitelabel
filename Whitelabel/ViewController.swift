4f919ca3961d3005d30f70b160639dee3f3d78ff
Artur Gurgul <artur.gurgul@redge.com>
2023-12-07 06:41:58 +0100

    Clean app

---
+++ Whitelabel/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Product
+//
+//  Created by Artur Gurgul on 07.12.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red
+    }
+
+
+}
+
